9b7cda6f0863e79b33c084b8835c22cbf33d08ed
Ehtasham Munib <emunib@outlook.com>
2021-12-30 18:21:52 -0600

    remove local data storage

--- server/utils/index.js
+++ server/utils/index.js
@@ -3,44 +3,6 @@ const qs = require('querystring');
 const fs = require('fs').promises;
 const {API_KEY: api_key} = process.env;
 
-const readShows = async (user) => {
-    const path = `./data/${user}.json`;
-    try {
-        await fs.access(path);
-    } catch (e) {
-        await writeShows(user, new Map());
-    }
-
-    return JSON.parse(await fs.readFile(path, 'utf8'), reviver);
-};
-
-const writeShows = async (user, data) => {
-    const path = `./data/${user}.json`;
-    return fs.writeFile(path, JSON.stringify(data, replacer), 'utf8');
-};
-
-
-// https://stackoverflow.com/questions/29085197/how-do-you-json-stringify-an-es6-map
-// Map object serialization and parsing
-function replacer(key, value) {
-    if (value instanceof Map) {
-        return {
-            dataType: 'Map', value: Array.from(value.entries())
-        };
-    } else {
-        return value;
-    }
-}
-
-function reviver(key, value) {
-    if (typeof value === 'object' && value !== null) {
-        if (value.dataType === 'Map') {
-            return new Map(value.value);
-        }
-    }
-    return value;
-}
-
 function formatBasicShow({id, name, overview, poster_path, vote_average}, following) {
     return {
         id,
@@ -99,4 +61,4 @@ function formatSeasons(seasons, showImg) {
 }
 
 
-module.exports = {readShows, writeShows, getShow, formatBasicShow, formatDetailedShow};
+module.exports = {getShow, formatBasicShow, formatDetailedShow};
